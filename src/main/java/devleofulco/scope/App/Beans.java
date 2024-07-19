465a0e697e4ba993ddadba8fb1e4cd9966ecdacf
DevLeoFulco <devleofulco@gmail.com>
2024-07-18 22:19:37 -0300

    Neste commit ja temos o Scope Prototype

--- src/main/java/devleofulco/scope/App/Beans.java
+++ src/main/java/devleofulco/scope/App/Beans.java
@@ -2,11 +2,13 @@ package devleofulco.scope.App;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class Beans {
 
     @Bean
+    @Scope("prototype")
     public Remetente remetente(){
         System.out.println("Criando um Objeto Remetente");
         Remetente remetente = new Remetente();
